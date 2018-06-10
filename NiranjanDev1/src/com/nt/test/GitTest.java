d84505164e617fca39f0bfcf7589f14a495cb1f9
Prabhat <Prabhat@DESKTOP-46PBUB6>
2018-06-10 15:02:03 +0530

    some mod1 >DEv1

--- NiranjanDev1/src/com/nt/test/GitTest.java
+++ NiranjanDev1/src/com/nt/test/GitTest.java
@@ -4,7 +4,8 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-
+		Math m=new Math();
+	    System.out.println(m.sum());
 	}
 
 }
---
+++ NiranjanDev1/src/com/nt/test/Math.java
@@ -0,0 +1,8 @@
+package com.nt.test;
+
+public class Math {
+	public int sum() {
+		return 100+200;
+	}
+
+}
